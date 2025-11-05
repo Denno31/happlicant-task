f0263cf8c0fec409e27a20cec3106d80606d3104
Denno31 <denniskyn80@gmail.com>
2025-11-05 21:55:18 +0300

    begin creating header component

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,9 @@
+import { Building2 } from "lucide-react";
+
+export default function Header() {
+  return (
+    <div className="sticky top-0 z-30 bg-white shadow-md border-b border-gray-200 -mx-4 sm:-mx-6 lg:-mx-8 px-4 sm:px-6 lg:px-8 py-6 mb-6">
+      
+    </div>
+  );
+}
